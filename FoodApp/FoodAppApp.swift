2795f57f3dcae62a2976f00dfb03262d87dd0a9d
RAM6IOS <58077232+RAM6IOS@users.noreply.github.com>
2021-04-29 17:46:56 +0100

    Initial Commit

---
+++ FoodApp/FoodAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodAppApp.swift
+//  FoodApp
+//
+//  Created by MAC on 29/4/2021.
+//
+
+import SwiftUI
+
+@main
+struct FoodAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
